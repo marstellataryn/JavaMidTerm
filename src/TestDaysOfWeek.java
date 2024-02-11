5674d723aadcfa52ca96625b3d0ff15049af4a61
marstellataryn <152827905+marstellataryn@users.noreply.github.com>
2024-02-11 14:40:27 -0500

    Update TestDaysOfWeek.java

--- src/TestDaysOfWeek.java
+++ src/TestDaysOfWeek.java
@@ -1,11 +1,11 @@
-Inport daysAndDates.DaysOfWeek;
+import daysAndDates.DaysOfWeek;
 
 public class TestDaysOfWeek {
 
 	public static void main(String[] args) {
 		System.out.println("Days Of week: ");
-		for (int i = 0;i < 8;i++) {
-			System.out.println("Number: " + i + "\tDay Of Week: " + DaysOfWeek.DayOfWeekStr(i) )
+		for (int i = 1;i < 8; i++) {
+			System.out.println("Number: " + i + "\tDay Of Week: " + DaysOfWeek.DayOfWeekStr(i));
 		}
 
 	}
