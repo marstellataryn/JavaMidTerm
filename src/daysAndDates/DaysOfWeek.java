30a65e8abc206ad586ff78020b8049de294fa2ed
marstellataryn <152827905+marstellataryn@users.noreply.github.com>
2024-02-11 14:31:48 -0500

    Add files via upload

---
+++ src/daysAndDates/DaysOfWeek.java
@@ -0,0 +1,30 @@
+package daysAndDates
+
+public class DaysOfWeek {
+	public static String DayOfWeekStr(int NumberOfDay) {
+		String dayStr = ""
+		switch (NumberOfDay) {
+			case 1:
+				dayStr = "Sunday";
+				break;
+			case 2:
+				dayStr = "Monday";
+				break;
+			case 3:
+				dayStr = "Tuesday"
+				break;
+			case 4:
+				dayStr = "Wednesday";
+			case 5:
+				dayStr = "Thursday";
+				break;
+			case 6:
+				dayStr = "Thursday";
+				break;
+			case 7:
+				dayStr = "Saturday";
+				break;
+		}
+		
+	}
+}
